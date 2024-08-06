67b430192ae39e3687572eafb497295cfc9c9db9
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-06 15:39:29 +0200

    Add template for product card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="product-card" @click="navigateToDetail">
+    <img :src="product.image" alt="Product Image" class="product-image" />
+    <h3>{{ product.title }}</h3>
+    <p>${{ product.price }}</p>
+    <p>{{ product.category }}</p>
+    <p>Ratings: {{ product.rating.rate }}</p>
+    <p>Reviews: {{ product.rating.count }}</p>
+        <!-- Buttons for Adding to Cart and Wishlist -->
+        <button @click="handleAddToCart" class="add-button">Add to Cart</button>
+    <button @click="handleAddToWishlist" class="add-button">Add to Wishlist</button>
+  </div>
+</template>
