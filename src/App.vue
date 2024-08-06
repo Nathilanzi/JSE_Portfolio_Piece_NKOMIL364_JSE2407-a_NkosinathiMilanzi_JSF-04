493ce1a686c35922c3567f2ba2aee57f46f721ca
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-06 15:48:16 +0200

    Add route to navbar

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <div id="app"> -->
+    <!-- <Header /> -->
+    <Layout />
+    <!-- <Filter/> -->
+    <!-- <Wishlist /> -->
+    <!-- <Cart /> -->
+    <ProductList />
+    <!-- <LoadingSpinner /> -->
+  <!-- </div> -->
+</template>
+
+<script setup>
+// import Header from './components/Header.vue';
+import Layout from './components/Layout.vue';
+// import Wishlist from './components/Wishlist.vue';
+// import Cart from './components/Cart.vue';
+// import ProductList from './components/ProductList.vue';
+// import Filter from './components/Filter.vue';
+// import LoadingSpinner from './components/LoadingSpinner.vue';
+</script>
+
+<style scoped>
+/* Add your global styles here */
+</style>
