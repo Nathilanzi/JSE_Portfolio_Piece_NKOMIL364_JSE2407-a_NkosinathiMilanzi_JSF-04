ddd9c201b6e61a46b62f97442e1008ff4c4590e6
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-06 15:36:30 +0200

    Add template for filter

---
+++ src/components/Filter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <select v-model="selectedCategory" @change="onCategoryChange">
+      <option v-for="category in categories" :key="category">
+        {{ category }}
+      </option>
+    </select>
+
+    <div class="grid">
+      <ProductCard
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
+  </div>
+</template>
