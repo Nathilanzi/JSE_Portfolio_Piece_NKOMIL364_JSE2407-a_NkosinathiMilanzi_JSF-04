d8fdbd61f6e9a07ced3999ca7a6ca038ad675c6c
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-16 16:55:58 +0200

    Add template that uses directives and buttons for removing comparison items

---
+++ src/components/ComparisonPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div v-if="isAuthenticated">
+      <h1>Comparison Page</h1>
+      <button @click="clearComparison">Clear All</button>
+      <table v-if="comparisonItems.length > 0">
+        <thead>
+          <tr>
+          <th v-for="item in comparisonItems" :key="`title-${item.id}`">{{ item.title }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td v-for="item in comparisonItems" :key="'image-${item.id}'">
+              <img :src="item.image" alt="Product Image" />
+            </td>
+          </tr>
+          <tr>
+            <td v-for="item in comparisonItems" :key="'description-${item.id}'">{{ item.description }}</td>
+          </tr>
+          <tr>
+            <td v-for="item in comparisonItems" :key="'price-${item.id}'">${{ item.price }}</td>
+          </tr>
+          <tr>
+            <td v-for="item in comparisonItems" :key="'rating-${item.id}'">{{ item.rating }}</td>
+          </tr>
+          <tr>
+            <td v-for="item in comparisonItems" :key="'remove-${item.id}'">
+              <button @click="removeItem(item.id)">Remove</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <p v-else>No items to compare.</p>
+    </div>
+    <p v-else>You must be logged in to view this page.</p>
+  </template>
