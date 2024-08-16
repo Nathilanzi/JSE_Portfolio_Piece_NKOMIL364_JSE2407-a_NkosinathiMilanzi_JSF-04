1cc518376f5730220fe4e319cea833d4f14e8b12
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-16 16:30:02 +0200

    add filtering html with buttons

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>
-    <select v-model="selectedCategory" @change="onCategoryChange">
-      <option v-for="category in categories" :key="category">
-        {{ category }}
-      </option>
+  <div class="filter-container">
+    <div class="filters">
+      <div class="filter-group">
+        <label for="category-filter">Category:</label>
+        <select id="category-filter" v-model="selectedCategory" @change="updateFiltersAndSort">
+          <option value="">All Categories</option>
+          <option v-for="category in categories" :key="category" :value="category">{{ category }}</option>
+        </select>
+      </div>
+      <div class="filter-group">
+        <label for="price-filter">Sort by Price:</label>
+        <select id="price-filter" v-model="selectedSort" @change="updateFiltersAndSort">
+          <option value="default">Default</option>
+          <option value="lowToHigh">Low to High</option>
+          <option value="highToLow">High to Low</option>
     </select>
+      </div>
+      <button @click="resetFilters" class="reset-button">Reset Filters</button>
+    </div>
 
     <div class="grid">
       <ProductCard
