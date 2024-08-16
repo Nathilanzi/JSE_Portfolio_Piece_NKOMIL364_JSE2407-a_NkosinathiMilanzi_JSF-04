1e64a5fa595c25dc386b009e201757c4426f6d41
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-16 16:25:01 +0200

    install pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 // main.js
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(createPinia());
+app.use(router)
+app.mount('#app');
+
+// createApp(App).mount('#app');
