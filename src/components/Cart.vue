0e3ed716147b5d81a4f406f9900438059cfe8fb4
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-16 16:48:01 +0200

    Add template with remove and clear cart buttons

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,11 +1,27 @@
-<!-- <template>
-  <div>
-    <h2>Cart</h2>
-    <ul>
-      <li v-for="item in cart" :key="item.id">
-        {{ item.title }} - {{ item.quantity }}
-      </li>
-    </ul>
+<template>
+  <div v-if="isAuthenticated" class="cart-container">
+    <h2>Your Shopping Cart</h2>
+    <div v-if="cartItems.length === 0">
+      <p>Your cart is empty.</p>
+    </div>
+    <div v-else>
+      <div v-for="item in cartItems" :key="item.id" class="cart-item">
+        <img :src="item.image" :alt="item.title" />
+        <div class="item-details">
+          <h3>{{ item.title }}</h3>
+          <p>${{ item.price | currency }}</p>
+          <div class="quantity">
+            <label for="quantity">Quantity:</label>
+            <input type="number" v-model.number="item.quantity" min="1" @change="updateCartQuantity(item)" />
+          </div>
+          <button @click="removeItem(item.id)">Remove</button>
+        </div>
+      </div>
+      <div class="cart-total">
+        <p>Total: ${{ cartTotal | $ }}</p>
+        <button @click="clearCart">Clear Cart</button>
+      </div>
+    </div>
   </div>
 </template>
 
