e36586c3a5345ffc394ad8b3cfb6b3b23ae04555
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-18 14:06:24 +0200

    Update routes

--- src/router/index.js
+++ src/router/index.js
@@ -63,4 +63,17 @@ const router = createRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token');
+
+  if (to.matched.some(record => record.meta.requiresAuth) && !token) {
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath },
+    });
+  } else {
+    next();
+  }
+});
+
 export default router;
