8c4ef4fd37d6ea954eee61e348d04c20f123409b
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-18 14:05:44 +0200

    Add import logic

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../components/Login.vue';
 import ProductList from '../components/ProductList.vue';
 import ProductDetails from '../components/ProductDetails.vue';
-// import Cart from '../components/Cart.vue';
-// import WishList from '../components/WishList.vue';
+import WishList from '../components/WishList.vue';
 import LoadingSpinner from '../components/LoadingSpinner.vue';
+import Cart from '../components/Cart.vue';
+import Comparison from '../components/ComparisonPage.vue';
 
 const routes = [
   {
