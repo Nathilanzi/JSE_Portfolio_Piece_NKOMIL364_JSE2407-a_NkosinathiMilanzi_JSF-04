71b52cb008698d84bbf6d9354e63e7b0fd69d9e0
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-18 14:07:33 +0200

    add wishlistStore to handle updating wishlist logic

---
+++ src/Store/WishlistStore.js
@@ -0,0 +1,35 @@
+// src/Store/WishListStore.js
+import { defineStore } from 'pinia';
+
+export const useWishListStore = defineStore('wishlist', {
+  state: () => ({
+    wishlistItems: [],
+  }),
+  actions: {
+    loadWishlist() {
+      const storedWishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+      this.wishlistItems = storedWishlist;
+    },
+    addItem(item) {
+      this.wishlistItems.push(item);
+      localStorage.setItem('wishlist', JSON.stringify(this.wishlistItems));
+    },
+    removeItem(itemId) {
+      this.wishlistItems = this.wishlistItems.filter(item => item.id !== itemId);
+      localStorage.setItem('wishlist', JSON.stringify(this.wishlistItems));
+    },
+    updateWishlist(item) {
+        const existingItem = this.wishlistItems.find(i => i.id === item.id);
+        if (existingItem) {
+          Object.assign(existingItem, item);
+        }
+        localStorage.setItem('wishlist', JSON.stringify(this.wishlistItems));
+      },
+    clearWishlist() {
+      this.wishlistItems = [];
+      localStorage.setItem('wishlist', JSON.stringify(this.wishlistItems));
+    },
+  },
+});
+
+
