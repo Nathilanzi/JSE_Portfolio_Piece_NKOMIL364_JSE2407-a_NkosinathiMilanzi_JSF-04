0addbe5e4793d86eb899d9fcfede3219a32e3f3d
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-18 14:03:42 +0200

    Update template with functions for clearing wishlist

--- src/components/WishList.vue
+++ src/components/WishList.vue
@@ -1,11 +1,27 @@
-<!-- <template>
-  <div>
-    <h2>Wishlist</h2>
-    <ul>
-      <li v-for="item in wishlist" :key="item.id">
-        {{ item.title }}
-      </li>
-    </ul>
+<template>
+  <div v-if="isAuthenticated" class="wishlist-container">
+    <h1>Your Wishlist</h1>
+    <div v-if="wishlistItems.length === 0">
+      <p>Your wishlist is empty.</p>
+    </div>
+    <div v-else>
+      <div v-for="item in wishlistItems" :key="item.id" class="wishlist-item">
+        <img :src="item.image" :alt="item.title" />
+        <div class="item-details">
+          <h3>{{ item.title }}</h3>
+          <p>${{ item.price | currency }}</p>
+        </div>
+        <div class="quantity">
+            <label for="quantity">Quantity:</label>
+            <input type="number" v-model.number="item.quantity" min="1" @change="updateWishlisQuantity(item)" />
+          </div>
+        <button @click="removeItem(item.id)">Remove</button>
+      </div>
+      <div class="wishlist-total">
+        <p>Total: ${{ wishlistTotal | $ }}</p>
+        <button @click="clearWishlist">Clear Wishlist</button>
+      </div>
+    </div>
   </div>
 </template>
 
