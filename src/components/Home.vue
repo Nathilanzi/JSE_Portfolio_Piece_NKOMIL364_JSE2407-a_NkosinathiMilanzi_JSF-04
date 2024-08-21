cb7121226684f1948539fe9d090b482c475534eb
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-21 15:15:46 +0200

    Create homepage for discount carousel

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="background">
+      <h1>Welcome to Our Store</h1>
+      <DiscountCarousel />
+      <!-- Other home page content -->
+    </div>
+  </template>
+  
+  <script setup>
+  import DiscountCarousel from './DiscountCarousel.vue';
+  </script>
+  
