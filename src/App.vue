98f73cd56b3db60c285a937bd38f9a279acf4cc5
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-21 14:37:30 +0200

    add discouunt details to main app

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <!-- <div id="app"> -->
     <!-- <Header /> -->
     <Layout />
+    <!-- <DiscountCarousel/> -->
+    <DiscountDetails/>
     <!-- <ComparisonPage /> -->
     <!-- <Filter/> -->
     <!-- <Wishlist /> -->
@@ -14,12 +16,22 @@
 <script setup>
 // import Header from './components/Header.vue';
 // import ComparisonPage from './components/ComparisonPage.vue';
+// import DiscountCarousel from './components/DiscountCarousel.vue';
+import DiscountDetails from './components/DiscountDetails.vue';
 import Layout from './components/Layout.vue';
+// import { createApp } from 'vue';
+// import { createPinia } from 'pinia';
+// import router from './router';
+// import App from './App.vue';
 // import Wishlist from './components/Wishlist.vue';
 // import Cart from './components/Cart.vue';
 // import ProductList from './components/ProductList.vue';
 // import Filter from './components/Filter.vue';
 // import LoadingSpinner from './components/LoadingSpinner.vue';
+// const app = createApp(App);
+// app.use(createPinia());
+// app.use(router);
+// app.mount('#app');
 </script>
 
 <style scoped>
