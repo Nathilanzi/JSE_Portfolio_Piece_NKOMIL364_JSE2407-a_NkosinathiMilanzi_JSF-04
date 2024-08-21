5361350914f5b4757b913c9013428902ba5e30d6
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-21 15:24:37 +0200

    add discounts into the template and update styles to include dark mode filtering

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -6,6 +6,10 @@
     <p>{{ product.category }}</p>
     <p>Ratings: {{ product.rating.rate }}</p>
     <p>Reviews: {{ product.rating.count }}</p>
+    <p v-if="product.discountedPrice" class="original-price">${{ product.price.toFixed(2) }}</p>
+    <p v-if="product.discountedPrice" class="discounted-price">${{ product.discountedPrice }}</p>
+    <p v-if="product.discountedPrice" class="discount-percentage">-{{ product.discount }}%</p>
+    <p v-else>${{ product.price.toFixed(2) }}</p>
         <!-- Buttons for Adding to Cart, Wishlist and Comparison -->
         <button @click.stop="addToCart" class="add-button">Add to Cart</button>
         <button @click.stop="addToWishlist" class="add-button">Add to Wishlist</button>
@@ -79,34 +83,76 @@ const navigateToDetail = () => {
 
 <style scoped>
 .product-card {
-  border: 1px solid #ddd;
+  border: 4px solid var(--border-color);
   padding: 16px;
   cursor: pointer;
-  transition: box-shadow 0.2s ease;
+  transition: box-shadow 0.3s ease-in-out;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  background-color: var(--background-color);
+  color: var(--text-color);
 }
 
-.product-card:hover {
+/* .product-card:hover {
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
+  background-color: var(--hover-background-color);
+} */
 
 .product-image {
   width: 100%;
-  height: auto;
   margin-bottom: 16px;
+  object-fit: contain;
+}
+
+.button-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: auto;
 }
 
 .add-button {
-  margin: 10px;
-  padding: 10px 20px;
-  background-color: #3498db;
+  padding: 10px;
+  background-color: #d61414;
   color: white;
   border: none;
   border-radius: 5px;
   cursor: pointer;
-  transition: background-color 0.3s ease;
+  transition: background-color 0.3s ease-in-out;
+  flex: 1;
+  margin: 0 5px;
+  max-width: 120px;
+  text-align: center;
 }
 
 .add-button:hover {
-  background-color: #2980b9;
+  background-color: #800606;
+}
+
+.card {
+  padding: 16px;
+  border: 1px solid var(--border-color);
+  border-radius: 8px;
+  background-color: var(--background-color);
+  color: var(--text-color);
+}
+
+/* Light mode colors */
+:root {
+  --border-color: #261d5c;
+  --background-color: bisque;
+  --text-color: #000;
+  --hover-background-color: #ffe4c4;
+}
+
+/* Dark mode colors */
+.dark-mode {
+  --border-color: #1b1340;
+  --background-color: #3e3e3e;
+  --text-color: #fff;
+  --hover-background-color: #5a5a5a;
 }
 </style>
+
