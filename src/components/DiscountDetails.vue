2ebe750441a9ebaa2e0a37918896b97c69843d81
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-21 15:06:51 +0200

    Add template for discount details, with original prices and discounts along with percentages

---
+++ src/components/DiscountDetails.vue
@@ -0,0 +1,10 @@
+<template>
+    <div v-if="product">
+      <img :src="product.image" :alt="product.title" />
+      <h1>{{ product.title }}</h1>
+      <p class="original-price">${{ product.price.toFixed(2) }}</p>
+      <p class="discounted-price">${{ product.discountedPrice }}</p>
+      <p class="discount-percentage">-{{ product.discount }}%</p>
+      <p class="sale-end-date">Sale ends on: {{ saleEndDate.toLocaleDateString() }}</p>
+    </div>
+  </template>
